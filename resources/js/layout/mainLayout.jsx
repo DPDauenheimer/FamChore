1d23af58b2ffefb08da1ea03b3269136d7699d9e
ddauenheimer <d.dauenheimer@11ts-e.com>
2023-07-08 21:44:20 +0200

    addded app icon

--- resources/js/layout/mainLayout.jsx
+++ resources/js/layout/mainLayout.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '../elements/head'
 
 export default function mainLayout(props) {
-  return (
-    <main>
+  return [
+    <header key="1">
+
+    </header>,
+    <main key="2">
         {props.children}
     </main>
-  )
+  ]
 }
